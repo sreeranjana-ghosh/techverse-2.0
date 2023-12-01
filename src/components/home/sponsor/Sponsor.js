0896392b1d343dd0003352c0ca91e42e70b684c3
Parthiv-Sarker <parthivsdevelopment@gmail.com>
2023-12-01 20:19:57 +0530

    final

--- src/components/home/sponsor/Sponsor.js
+++ src/components/home/sponsor/Sponsor.js
@@ -5,12 +5,48 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Sponsor = () => {
+	const sliderSettings = {
+		dots: true,
+		infinite: true,
+		speed: 1000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		fade:true
+		
+	};
 	return (
 		<div className="w-screen flex flex-col justify-center items-center ">
 			<h1 className="text-4xl md:text-7xl font-roboto font-extrabold text-white">
 				OUR SPONSORS
 			</h1>
-			<marquee>
+			<Slider {...sliderSettings} className="w-40 md:w-96 h-96 p-4 mt-20">
+			<div className="relative w-60 h-28 md:w-40 md:h-40">
+						<Image src="/sponsers/GNIT.png" alt="logo" fill />
+					</div>
+					<div className="relative w-96 h-28 md:w-40 md:h-40">
+						<Image src="/sponsers/JIS.png" alt="logo" fill />
+					</div>
+					<div className="relative w-96 h-16 md:w-60 md:h-28">
+						<Image src="/sponsers/council.png" alt="logo" fill />
+					</div>
+					<div className="relative w-96 h-10 md:w-80 md:h-20">
+						<Image
+							src="/sponsers/teenageresports.png"
+							alt="logo"
+							fill
+						/>
+					</div>
+					<div className="relative w-96 h-10 md:w-56 md:h-20">
+						<Image src="/sponsers/edugraph.jpg" alt="logo" fill />
+					</div>
+					<div className="relative w-96 h-16 md:w-52 md:h-32">
+						<Image src="/sponsers/techghosh.png" alt="logo" fill />
+					</div>
+			</Slider>
+			{/* <marquee>
 				<div className="w-[200vw] md:w-screen relative flex justify-center items-center gap-14 mt-20">
 					<div className="relative w-60 h-28 md:w-40 md:h-40">
 						<Image src="/sponsers/GNIT.png" alt="logo" fill />
@@ -18,7 +54,7 @@ const Sponsor = () => {
 					<div className="relative w-96 h-20 md:w-40 md:h-40">
 						<Image src="/sponsers/JIS.png" alt="logo" fill />
 					</div>
-          <div className="relative w-96 h-16 md:w-60 md:h-28">
+					<div className="relative w-96 h-16 md:w-60 md:h-28">
 						<Image src="/sponsers/council.png" alt="logo" fill />
 					</div>
 					<div className="relative w-96 h-10 md:w-80 md:h-20">
@@ -35,7 +71,7 @@ const Sponsor = () => {
 						<Image src="/sponsers/techghosh.png" alt="logo" fill />
 					</div>
 				</div>
-			</marquee>
+			</marquee> */}
 		</div>
 	);
 };
