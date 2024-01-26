a89c41351894ab3e470a0544384f7cc5f0c27278
Parthiv-Sarker <parthivsdevelopment@gmail.com>
2024-01-26 21:20:31 +0530

    Add Close event functionality2.

--- src/app/events/eventcard/EventCard.js
+++ src/app/events/eventcard/EventCard.js
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./styles.module.css";
 import Image from "next/image";
 
+// addd
+
 const EventCard = ({ imgUrl, eventName, details, closeEvent }) => {
 	return (
 		<div className={styles.wrapper}>
